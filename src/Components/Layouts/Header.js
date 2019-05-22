c380b000379a3c4f606a6a430f4b523e8a894807
ScherbakovAlexey <scherbakovalexey@mail.ru>
2019-05-22 21:52:32 +0300

    'initial'

---
+++ src/Components/Layouts/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+
+export default props => (
+  <AppBar>
+    <Toolbar>
+      <Typography variant="headline" color="inherit">
+        h5. Heading
+      </Typography>
+    </Toolbar>
+  </AppBar>
+);
