446d169d31423e32fc3e3fab8ebbf00331445243
ScherbakovAlexey <scherbakovalexey@mail.ru>
2019-05-22 22:34:46 +0300

    grid

--- src/Components/App.js
+++ src/Components/App.js
@@ -8,9 +8,8 @@ export default class extends Component {
       <Fragment>
         <Header />
         <Exercises />
-        <Exercises />
-        <Exercises />
         <Footer />
+        <Exercises />
       </Fragment>
     );
   }
--- src/Components/Exercises/index.js
+++ src/Components/Exercises/index.js
@@ -1,3 +1,16 @@
 import React from "react";
-
-export default props => <div>hi from ex</div>;
+import { Grid, Paper } from "@material-ui/core";
+export default props => (
+  <Grid container>
+    <Grid item sm>
+      <Paper style={{ padding: 20, marginBottom: 10, marginTop: 10 }}>
+        Left
+      </Paper>
+    </Grid>
+    <Grid item sm>
+      <Paper style={{ padding: 20, marginBottom: 10, marginTop: 10 }}>
+        Right
+      </Paper>
+    </Grid>
+  </Grid>
+);
